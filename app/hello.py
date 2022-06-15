fdd980c3986ac4012eb5b1fb9b0e73e76d7205af
gwrhyr <acrographia+dev@gmail.com>
2022-06-15 18:59:46 +0900

    database lesson terminated1

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, database_exists
-from flask import render_template
+from flask import render_template, request, redirect
+from datetime import datetime
+import pytz
 
 # Intatiate Flask and SQLAlchemy
 app = Flask(__name__)
@@ -13,6 +15,43 @@ if not database_exists(db_url):
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
-@app.route("/")
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(300), nullable=False)
+    createdat = db.Column(db.DateTime, nullable=False,
+                                        default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('hello.html')
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('hello.html', posts = posts)
+    
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title =  request.form.get('title')
+        body =  request.form.get('body')
+
+        post = Post(title=title, body=body)
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('create.html')
+
+@app.route('/<int:id>/update', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title =  request.form.get('title')
+        post.body =  request.form.get('body')
+
+        db.session.commit()
+        return redirect('/')
+
+
