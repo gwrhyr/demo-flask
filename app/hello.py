1d1477b0d65c69d79b5a23b341dad248b02e0b26
gwrhyr <acrographia+dev@gmail.com>
2022-06-16 12:51:00 +0900

    Terminated whole the lesson

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
 from sqlalchemy_utils import create_database, database_exists
 from flask import render_template, request, redirect
+from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from datetime import datetime
 import pytz
 
@@ -14,6 +18,8 @@ if not database_exists(db_url):
 
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SECRET_KEY'] = os.urandom(24)
+bootstrap = Bootstrap(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,13 +28,60 @@ class Post(db.Model):
     createdat = db.Column(db.DateTime, nullable=False,
                                         default=datetime.now(pytz.timezone('Asia/Tokyo')))
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+@login_required
+def index():
     if request.method == 'GET':
         posts = Post.query.all()
-        return render_template('hello.html', posts = posts)
+        return render_template('index.html', posts = posts)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username =  request.form.get('username')
+        password =  request.form.get('password')
+
+        user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    else:
+        return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username =  request.form.get('username')
+        password =  request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
     
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == 'POST':
         title =  request.form.get('title')
@@ -43,6 +96,7 @@ def create():
         return render_template('create.html')
 
 @app.route('/<int:id>/update', methods=['GET', 'POST'])
+@login_required
 def update(id):
     post = Post.query.get(id)
     if request.method == 'GET':
@@ -53,5 +107,14 @@ def update(id):
 
         db.session.commit()
         return redirect('/')
+
+@app.route('/<int:id>/delete', methods=['GET'])
+@login_required
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
 
 
