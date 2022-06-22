1f238c5701a975162de13e2452670befecb3d135
gwrhyr <acrographia+dev@gmail.com>
2022-06-23 00:27:31 +0900

    Modified connextion problems(environment value and timezone)

--- app/hello.py
+++ app/hello.py
@@ -19,7 +19,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(300), nullable=False)
-    create_at = db.Column(db.DateTime, nullable=False,default=datetime.now(pytz.timezone(Asia/Tokyo)))
+    create_at = db.Column(db.DateTime, nullable=False,default=datetime.now(pytz.timezone('Asia/Tokyo')))
 
 
 @app.route("/")
